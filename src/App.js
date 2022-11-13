82f53e7a6abb0fec7bf8f4e354e0432914a87dce
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-11-13 21:29:57 +0100

    chore:created hero component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 // import { BrowserRouter as Router, Routes, } from "react-router-dom";
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import HeroImg from "../assets/HeroImg.svg";
+import MBToken from "../assets/MBToken.svg";
+import MetaMask from "../assets/MetaMask.svg";
+import OpenSea from "../assets/OpenSea.svg";
 
 const Hero = () => {
-  return <div>hero</div>;
+  return (
+    <main>
+      <div className="flex px-24 w-full mt-32 items-center justify-between gap-12">
+        <div className="w-1/2  gap-12 flex flex-col">
+          <h1 className="text-6xl text-[#434343]">
+            Rent a <span className="text-[#A02279]">Place </span> away from{" "}
+            <br /> <span className="text-[#A02279]">Home </span> in the{" "}
+            <span className="text-[#A02279]">Metaverse </span>
+          </h1>
+          <p className="text-2xl text-[#434343]">
+            we provide you access to luxury and affordable houses in the
+            metaverse, get a chance to turn your imagination to reality at your
+            comfort zone
+          </p>
+          <div className="flex ">
+            <input
+              className="border-2 border-r-0 rounded-r-none rounded-lg border-[#A3A3A3] py-4 px-4 bg-white w-full"
+              type="email"
+              name=""
+              id=""
+              placeholder="Search for Location"
+            />
+            <button className="rounded-l-none rounded-lg py-4 px-2  w-2/3 md:w-2/5 bg-[#A02279] text-white">
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="">
+          <img src={HeroImg} alt="hero" />
+        </div>
+      </div>
+      <div className="bg-[#A02279] mt-24 h-16 flex gap-64 pt-4 items-center justify-around">
+        <img src={MBToken} alt="" />
+        <img src={MetaMask} alt="" />
+        <img src={OpenSea} alt="" />
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
